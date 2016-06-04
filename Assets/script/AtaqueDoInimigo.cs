08afb79a9337f3ae41273594fa7b5271903d81c0
lluisfelipe <f.felipe.12@hotmail.com>
2016-06-04 11:33:31 -0300

    mais alguma coisa

---
+++ Assets/script/AtaqueDoInimigo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtaqueDoInimigo : MonoBehaviour {
+	public int forca;
+	public float tempo;
+	public float tempoDecorrido;
+	public Inimigo inimigo;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tempoDecorrido += Time.deltaTime;
+		if (tempoDecorrido >= tempo) {
+			inimigo.podeAtacar ();
+			Destroy(this.gameObject);
+		}
+	
+	}
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.GetComponent<Player> () != null) {
+			coll.gameObject.GetComponent<Player> ().adicionarDano (forca);
+		}
+	}
+
+}
