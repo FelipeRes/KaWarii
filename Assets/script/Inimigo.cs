59e0e9b3aeb26357bffd525ddc3efb1f457d1adf
lluisfelipe <f.felipe.12@hotmail.com>
2016-06-04 10:22:34 -0300

    criei a base inimigo

---
+++ Assets/script/Inimigo.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inimigo : MonoBehaviour {
+	public int 			vida;
+	public GameObject 	player;
+	public GameObject 	ataque;
+	public int 			velocidade;
+	public float 		alcanceDeAtaque;
+	public float 		distancia;
+	public bool 		canAtack = true;
+	public int 			pontosEspecial;
+
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player.transform.position);
+		distancia = Vector3.Distance (this.transform.position, player.transform.position);
+		if (distancia > alcanceDeAtaque) {
+			transform.Translate (Vector3.forward * velocidade * Time.deltaTime);
+		}else if (distancia <= alcanceDeAtaque && canAtack) {
+			atacar ();
+		}
+	}
+	public void adicionarDano(int dano){
+		vida = vida - dano;
+		if (vida <= 0) {
+			morrer ();
+		}
+	}
+	public void atacar(){
+		canAtack = false;
+		Debug.Log ("atacou");
+		Instantiate (ataque);
+	}
+	public void podeAtacar(){
+		canAtack = true;
+	}
+	public void morrer(){
+		//FALTA A FUNÇAO DE ADICIONAR ESPECIAL AP PLAYER
+		Destroy(this.gameObject);
+	}
+}
