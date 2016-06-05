d7618e9c9944479b5eda267dc09249dc9c1bdff5
lluisfelipe <f.felipe.12@hotmail.com>
2016-06-05 17:40:49 -0300

    atualizando os objetos, colocando o prefab do power up

---
+++ Assets/Scripts/MonteDeLixo.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonteDeLixo : MonoBehaviour {
+	public int 			forcaDaEsplosao;
+	private bool 		esplodiu 		= false;
+	public GameObject[] objeto 			= new GameObject[4];
+	public float 		tempoDeDetruicao;
+	public GameObject 	powerUp;
+
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftControl)){
+			esplosao ();
+			Debug.Log ("Vc aperto ctrl");
+		}
+	}
+	public void esplosao(){
+		if (!esplodiu) {
+			esplodiu = true;
+			for (int cont = 0; cont < 4; cont++) {
+				if(objeto [cont] != null){
+					objeto [cont].GetComponent<Lixo> ().esplodindo (forcaDaEsplosao);
+				}
+			}
+			Invoke ("autoDestruicao", tempoDeDetruicao);
+			GameObject instantiate = GameObject.Instantiate (powerUp, transform.position, Quaternion.identity) as GameObject;
+
+		}
+	}
+	void autoDestruicao(){
+		Destroy (this.gameObject);
+	}
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.GetComponent<Ataque>() != null) {
+			esplosao ();
+		}
+	}
+}
