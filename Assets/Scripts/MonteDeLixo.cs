513babfb582079d25bc0b0d8983f08fd55a02c2e
lluisfelipe <f.felipe.12@hotmail.com>
2016-06-05 17:41:58 -0300

    opps
    esqueci de remover um bug

--- Assets/Scripts/MonteDeLixo.cs
+++ Assets/Scripts/MonteDeLixo.cs
@@ -11,12 +11,6 @@ public class MonteDeLixo : MonoBehaviour {
 	// Use this for initialization
 
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.LeftControl)){
-			esplosao ();
-			Debug.Log ("Vc aperto ctrl");
-		}
-	}
 	public void esplosao(){
 		if (!esplodiu) {
 			esplodiu = true;
